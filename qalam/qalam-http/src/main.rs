5be6e5d712a355fda8e85a3b45e102dbb249a9d1
UwUssimo Robinson <uwussimo@icloud.com>
2022-12-29 18:30:56 +0500

    [ADD] (qalam) monorepo created

--- qalam/qalam-http/src/main.rs
+++ qalam/qalam-http/src/main.rs
@@ -10,7 +10,6 @@ async fn main() -> std::io::Result<()> {
     let port = 3001;
     let address = "127.0.0.1";
     println!("ready - started server on {address}:{port}, url: http://localhost:{port}", address=address, port=port);
-
     HttpServer::new(|| {
         App::new()
             .service(hello)
