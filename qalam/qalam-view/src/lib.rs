9cf34b379c2544bee26560cd65e0daa3f3096277
Yūri Katsuki <yuri@katsuki.moe>
2023-01-05 11:10:50 +0500

    [FIX] removed unnecessary structs

--- qalam/qalam-view/src/lib.rs
+++ qalam/qalam-view/src/lib.rs
@@ -1,11 +1,2 @@
 pub mod tools;
 pub mod utils;
-
-use serde::{Deserialize, Serialize};
-
-#[derive(Serialize, Deserialize)]
-struct BetterResponse {
-    module: &'static str,
-    message: &'static str,
-    content: &'static str,
-}
