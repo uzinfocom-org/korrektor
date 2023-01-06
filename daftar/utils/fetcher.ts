4dfc6bbc99d5ba5b2456f92e0754532337cd1c31
Yūri Katsuki <yuri@katsuki.moe>
2023-01-06 17:45:33 +0500

    [FIX] fetcher any[] as reachable parameter

--- daftar/utils/fetcher.ts
+++ daftar/utils/fetcher.ts
@@ -2,5 +2,6 @@
 export const fetcher = (url: string) => fetch(url).then((res) => res.json());
 
 // @ts-ignore
-export const textFetcher = (...args) =>
-  fetch(...args).then((res) => res.text());
+export const textFetcher = (...args: any[]) =>
+    // @ts-ignore
+    fetch(...args).then((res) => res.text());
