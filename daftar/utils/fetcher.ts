fdc6709b39d45834e9fa134c3204375b42b73675
Yūri Katsuki <yuri@katsuki.moe>
2023-01-07 12:38:33 +0500

    [FIX] (format)

--- daftar/utils/fetcher.ts
+++ daftar/utils/fetcher.ts
@@ -3,5 +3,5 @@ export const fetcher = (url: string) => fetch(url).then((res) => res.json());
 
 // @ts-ignore
 export const textFetcher = (...args: any[]) =>
-    // @ts-ignore
-    fetch(...args).then((res) => res.text());
+  // @ts-ignore
+  fetch(...args).then((res) => res.text());
