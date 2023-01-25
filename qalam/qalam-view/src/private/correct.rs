ac148c075aeadd4d93a767d028f11de3b64c3325
Crystal Kate <crystalnyan@icloud.com>
2023-01-25 16:04:49 +0500

    [FIX] (correct) add authentication, fix messages
    added BearerAuth for methods other than main; fixed messages containing method paths

--- qalam/qalam-view/src/private/correct.rs
+++ qalam/qalam-view/src/private/correct.rs
@@ -1,6 +1,8 @@
+use crate::auth::middleware;
 use actix_web::{get, web, HttpResponse};
-use serde_json::json;
+use actix_web_httpauth::extractors::bearer::BearerAuth;
 use korrektor_rs_private;
+use serde_json::json;
 
 #[get("/correct")]
 pub async fn main() -> HttpResponse {
@@ -8,37 +10,46 @@ pub async fn main() -> HttpResponse {
 }
 
 #[get("/correct/content/{lang}/{content}")]
-pub async fn content(path: web::Path<(String, String)>) -> HttpResponse {
+pub async fn content(path: web::Path<(String, String)>, auth: BearerAuth) -> HttpResponse {
     let (language, content) = path.into_inner();
     let process = korrektor_rs_private::corrector::get_correction_suggestions(&content, &language);
 
-    HttpResponse::Ok().json(json!({
-        "message": "private/correct",
+    middleware(
+        HttpResponse::Ok().json(json!({
+        "message": "private/correct/content",
         "query": content,
         "content": process
-    }))
+    })),
+        auth,
+    )
 }
 
 #[get("/correct/modifiers/{text_content}")]
-pub async fn modifiers(path: web::Path<String>) -> HttpResponse {
+pub async fn modifiers(path: web::Path<String>, auth: BearerAuth) -> HttpResponse {
     let text_content = path.into_inner();
     let process = korrektor_rs_private::corrector::remove_modifiers(&text_content);
 
-    HttpResponse::Ok().json(json!({
-        "message": "private/correct",
+    middleware(
+        HttpResponse::Ok().json(json!({
+        "message": "private/correct/modifiers",
         "query": text_content,
         "content": process
-    }))
+    })),
+        auth,
+    )
 }
 
 #[get("/correct/syntax/{text_content}")]
-pub async fn syntax(path: web::Path<String>) -> HttpResponse {
+pub async fn syntax(path: web::Path<String>, auth: BearerAuth) -> HttpResponse {
     let text_content = path.into_inner();
     let process = korrektor_rs_private::corrector::correct(&text_content);
 
-    HttpResponse::Ok().json(json!({
-        "message": "private/correct",
+    middleware(
+        HttpResponse::Ok().json(json!({
+        "message": "private/correct/syntax",
         "query": text_content,
         "content": process
-    }))
+    })),
+        auth,
+    )
 }
