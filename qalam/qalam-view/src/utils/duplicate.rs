c7b407138e936d00ba37f41cedd6bfbaa0f27133
Crystal Kate <crystalnyan@icloud.com>
2023-01-27 19:31:20 +0500

    add test for /duplicate/{content}

--- qalam/qalam-view/src/utils/duplicate.rs
+++ qalam/qalam-view/src/utils/duplicate.rs
@@ -19,3 +19,28 @@ pub async fn content(path: web::Path<String>) -> HttpResponse {
         "content": process
     }))
 }
+
+#[cfg(test)]
+mod tests {
+    use std::os::macos::raw::stat;
+    use super::*;
+    use actix_web::body::MessageBody;
+    use actix_web::http;
+
+    #[actix_web::test]
+    async fn test_index_ok() {
+        let text_content = "hello sam hello";
+        let process = duplicates::remove(text_content);
+
+        let response = json!({
+        "message": "utils/duplicate",
+        "query": text_content,
+        "content": process
+    });
+
+        let static_json =
+            "{\"content\":\"hello sam\",\"message\":\"utils/duplicate\",\"query\":\"hello sam hello\"}";
+
+        assert_eq!(serde_json::to_string(&response).unwrap(), static_json);
+    }
+}
