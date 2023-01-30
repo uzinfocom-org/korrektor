12e1c900f17f3c54a324023020cf2a36623d5fe6
Crystal Kate <crystalnyan@icloud.com>
2023-01-30 12:42:11 +0500

    [FIX] (tokenize) update to korrektor 0.2.2
    updated to korrektor 0.2.2 -> split_word to split_text; added test

--- qalam/qalam-view/src/tools/tokenize.rs
+++ qalam/qalam-view/src/tools/tokenize.rs
@@ -12,7 +12,7 @@ pub async fn main() -> HttpResponse {
 #[get("/tokenize/{content}")]
 pub async fn content(path: web::Path<String>, auth: BearerAuth) -> HttpResponse {
     let content = path.into_inner();
-    let process = tokenize::split_word(content.as_str());
+    let process = tokenize::split_text(content.as_str());
 
     middleware(
         HttpResponse::Ok().json(json!({
@@ -22,4 +22,27 @@ pub async fn content(path: web::Path<String>, auth: BearerAuth) -> HttpResponse 
         })),
         auth,
     )
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[actix_web::test]
+    async fn test_index_ok() {
+        let text_content = "singil chiroyli чиройли";
+        let process = tokenize::split_text(text_content);
+
+        let response = json!({
+        "message": "tools/tokenize",
+        "query": text_content,
+        "content": process
+    });
+
+
+        let static_json =
+            "{\"content\":\"si-ngil chi-roy-li чи-рой-ли\",\"message\":\"tools/tokenize\",\"query\":\"singil chiroyli чиройли\"}";
+
+        assert_eq!(serde_json::to_string(&response).unwrap(), static_json);
+    }
 }
