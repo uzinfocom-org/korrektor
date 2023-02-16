08f3ad162e3d7e1a6bf88fbfccc8a180c2c8c5b0
Crystal Kate <crystalnyan@icloud.com>
2023-02-16 09:49:22 +0500

    [FIX] specify token in auth middleware
    set specific token in auth middleware (to be changed later)

--- qalam/qalam-view/src/auth.rs
+++ qalam/qalam-view/src/auth.rs
@@ -3,7 +3,7 @@ use actix_web_httpauth::extractors::bearer::BearerAuth;
 use serde_json::json;
 
 pub fn middleware(req: HttpResponse, token: BearerAuth) -> HttpResponse {
-    if !token.token().is_empty() {
+    if token.token() == "D2~0$oau@Zp{Wy06B!Ye$DmUT(P1Q{$t" {
         println!("TOKEN: {}", token.token());
         req
     } else {
